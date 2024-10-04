107e1654adefd183ac8600ed2a32ca382e11618b
Tom Marks <tom@tommarks.xyz>
2024-10-04 11:36:49 +1000

    src/game/player.c: floor resolve, jump buffer

--- src/game/player.c
+++ src/game/player.c
@@ -17,7 +17,7 @@ static const float GROUND_ACCEL = 37.2;
 static const float GRAVITY = 98;
 static const float TERMINAL_VELOCITY = 288.87;
 static const float FRICTION = 55.1;
-static const float FOOT_OFFSET_Y = 0.1;
+static const float FOOT_OFFSET_Y = 0.001;
 static const float FOOT_OFFSET_X = 2.4;
 static const float JUMP_SPEED = -120;
 
@@ -44,6 +44,7 @@ typedef struct playerdata {
   enum pstate state;
   int pushing_left;
   int pushing_right;
+  int has_jump_buffered;
 } playerdata;
 
 static int player_draw(struct entity *player, struct gamectx *ctx) {
@@ -63,8 +64,14 @@ static int player_update(struct entity *player, struct gamectx *ctx, float dt) {
     impulse_x -= 1;
   }
   int jump_impulse = button_held(BUTTON_X);
-  int do_jump = button_pressed(BUTTON_X);
   playerdata *pd = (playerdata *) player->data;
+  if (button_pressed(BUTTON_X)) {
+    pd->has_jump_buffered = 1;
+  }
+  if (button_released(BUTTON_X)) {
+    pd->has_jump_buffered = 0;
+  }
+  
 
   if (fabs(impulse_x) > EPSILON) {
     if (fabs(pd->vx) > EPSILON && (signof(impulse_x) != signof(pd->vx))) {
@@ -92,14 +99,18 @@ static int player_update(struct entity *player, struct gamectx *ctx, float dt) {
   } else {
     float foot_x0 = player->x + FOOT_OFFSET_X; 
     float foot_x1 = player->x + player->w - FOOT_OFFSET_X; 
-    float foot_y = player->y + player->y + FOOT_OFFSET_Y;
+    float foot_y = player->y + player->h + FOOT_OFFSET_Y;
     if (ctx_is_free_point(ctx, foot_x0, foot_y) && ctx_is_free_point(ctx, foot_x1, foot_y)) {
-      pd->state = FALL;
-      logdbg("not on ground, falling");
+      if (pd->state == STAND) {
+        pd->state = FALL;
+        pd->vy = 0;
+        logdbg("not on ground, falling @ [%f, %f]", player->x, player->y);
+      }
     } else {
-      if (do_jump) {
+      if (pd->has_jump_buffered) {
         pd->state = JUMP;
         pd->vy = JUMP_SPEED;
+        pd->has_jump_buffered = 0;
       }
     }
   }
@@ -133,11 +144,13 @@ static void collision_resolve(struct gamectx *ctx, struct entity *e, float dx, f
       int target_grid_place = (tgt_x+e->w) / ctx->collision.grid_size; 
       tgt_x = (target_grid_place * ctx->collision.grid_size) - e->w - 0.001;
       pd->pushing_right = 1;
+      pd->vx = 0;
       
     } else if (dx < 0) {
       int target_grid_place = tgt_x / ctx->collision.grid_size;
       tgt_x = (target_grid_place+1) * ctx->collision.grid_size;
       pd->pushing_left = 1;
+      pd->vx = 0;
     } 
   }
 
@@ -145,11 +158,12 @@ static void collision_resolve(struct gamectx *ctx, struct entity *e, float dx, f
   if (!ctx_is_free_box(ctx, tgt_x, tgt_y, e->w, e->h)) {
     if (dy > 0) {
       int target_grid_place = (tgt_y+e->h) / ctx->collision.grid_size; 
-      tgt_y = (target_grid_place * ctx->collision.grid_size) - e->h - FOOT_OFFSET_Y;
+      tgt_y = (target_grid_place * ctx->collision.grid_size) - e->h - (FOOT_OFFSET_Y*0.5);
       if (pd->state != STAND) {
         logdbg("hit the ground @ [%f, %f]", tgt_x, tgt_y);
       }
       pd->state = STAND;
+      pd->vy = 0;
     }
     // TODO: jump
   }
