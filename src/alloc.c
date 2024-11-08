dcdebf1fbdc217118c99dd488b41ae518ccb8ad7
Tom Marks <tom@tommarks.xyz>
2024-11-09 00:26:54 +1100

    src/alloc.c: add check for failed alloc

--- src/alloc.c
+++ src/alloc.c
@@ -1,10 +1,17 @@
 #include <string.h>
+#include <p2g/log.h>
 
 #include "alloc.h"
 
 void *alloc_from(struct allocator *a, size_t num, size_t size) {
   size_t total = num*size;
   void *out = a->alloc(a->state, num, size);
+#ifndef ALLOC_SKIP_CHECK
+  if (!out) {
+    logerr("alloc failed");
+    return 0;
+  }
+#endif
   return memset(out, 0, total);
 }
 
