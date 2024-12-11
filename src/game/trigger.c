3d2ce9e9d620e7e6dba1ee4f5ee0881868eafe33
Tom Marks <tom@tommarks.xyz>
2024-12-11 23:04:39 +1100

    src/game/trigger.c: not twice in a row

--- src/game/trigger.c
+++ src/game/trigger.c
@@ -4,6 +4,8 @@
 #include "task.h"
 #include "../levels/fmt.h"
 
+static uint32_t last_trigger_id = -1;
+
 int trigger_collides_point(struct trigger_area *a, float x, float y) {
   return (x >= a->position[0] 
       && y >= a->position[1]
@@ -12,6 +14,12 @@ int trigger_collides_point(struct trigger_area *a, float x, float y) {
 }
 
 int trigger_event(struct trigger_area *a, struct gamectx *ctx, struct levelctx *lvl) {
+  // TODO: use a real hash.
+  uint32_t hash = a->position[0]*7 + a->position[1]*2621 + a->size[0]*7639 + a->size[1]*7919;
+  if (hash == last_trigger_id) {
+    return 0;
+  }
+  last_trigger_id = hash;
   switch(a->kind) {
     case TRIGGER_LOAD_LEVEL:
       return task_submit(TASK_LOAD_LEVEL, a->arg);
